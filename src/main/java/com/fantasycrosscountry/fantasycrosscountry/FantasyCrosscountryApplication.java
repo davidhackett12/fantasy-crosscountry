0545c7f91ae68766a98538b8e8fe326281785a36
DHackett <davidhackett12@gmail.com>
2018-07-25 15:47:44 -0500

    Initial commit

---
+++ src/main/java/com/fantasycrosscountry/fantasycrosscountry/FantasyCrosscountryApplication.java
@@ -0,0 +1,12 @@
+package com.fantasycrosscountry.fantasycrosscountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyCrosscountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyCrosscountryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fantasycrosscountry/fantasycrosscountry/FantasyCrosscountryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fantasycrosscountry.fantasycrosscountry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantasyCrosscountryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
